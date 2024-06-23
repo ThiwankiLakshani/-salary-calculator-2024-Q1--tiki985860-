7a227230d96d0620b6871d4c730773a21a59824e
Thiwanki lakshani <95839292+tiki985860@users.noreply.github.com>
2024-06-23 10:08:00 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SalaryCalculator from './components/SalaryCalculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SalaryCalculator />
+    </div>
+  );
+}
+
+export default App;
